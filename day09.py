b821eaf70b74b017493ac908c64309affcf1a71a
Erik van Raalte <erik.vanraalte94@gmail.com>
2022-12-10 12:54:00 +0100

    prettify day09

--- day09.py
+++ day09.py
@@ -4,55 +4,42 @@ dirs = {
     "U": (0,1),
     "D": (0,-1),
 }
-h = (0,0)
-t = (0,0)
 
-NUM_KNOTS = 10
-# knots = [(0,0)]*NUM_KNOTS
-knots = {n: (0,0) for n in range(NUM_KNOTS)}
-locs = set()
-locs.add(t)
-
-
-def move_t(h,t):
+def calc_next_pos(h,t):
     tx, ty = t
     hx, hy = h
     # Get distance
     dx = hx-tx
     dy = hy-ty
-
-    if abs(dx) == 2 and dy ==0: # X movement
-        tx += 1 if dx > 0 else -1
-    elif abs(dy) == 2 and dx==0: # Y movement
-        ty += 1 if dy > 0 else -1    
-    elif abs(dx) <= 1 and abs(dy) <= 1:
-        pass # No need to move
-    else: # diagonal move
-        ty += 1 if dy > 0 else -1
-        tx +=1 if dx > 0 else -1
+    if abs(dx) <= 1 and abs(dy) <= 1:
+        return t# No need to move
+    tx += (dx > 0) - (dx < 0)
+    ty += (dy > 0) - (dy < 0)
     return tx, ty
 
+def get_coverage_of_tail(num_knots):
+    knots = {n: (0,0) for n in range(num_knots)}
+    locs = set([(0,0)])
+    for dir, distance in map(str.split,open("day09.txt").read().split('\n')):
+        x,y = dirs[dir]
+        for _ in range(int(distance)):
+            hx, hy = knots[0] # move 'real' head in the set direction
+            knots[0] = (hx+x, hy+y)
 
-for dir, distance in map(str.split,open("day09.txt").read().split('\n')):
-    x,y = dirs[dir]
-    # print(f"{dir} {distance}")
-    for _ in range(int(distance)):
-        hx, hy = knots[0] # move 'real' head in the set direction
-        # print(f"move H from {knots[0]} to {(hx+x, hy+y)}")
-        knots[0] = (hx+x, hy+y)
+            for knot_idx in range(1, num_knots): # Let the other knots follow
+                h = knots[knot_idx-1]
+                t = knots[knot_idx]
 
-        for knot_idx in range(1, NUM_KNOTS): # Let the other knots follow
-            h = knots[knot_idx-1]
-            t = knots[knot_idx]
-            # print(f"move T from {t} to {move_t(h,t)}")
-            t = move_t(h,t)
-            knots[knot_idx] = t
-            if knot_idx == NUM_KNOTS - 1:
-                locs.add(t) # Add end of robe to locs
-        pass
-        
-print(len(locs))
+                t = calc_next_pos(h,t)
+                knots[knot_idx] = t
+                if knot_idx == num_knots - 1:
+                    locs.add(t) # Add end of robe to locs
+            pass
 
+    return len(locs)
+
+print(get_coverage_of_tail(2))
+print(get_coverage_of_tail(10))
 
 
 
